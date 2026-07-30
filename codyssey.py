e7c01fe089c20795462f2325e3daa9b8fd47c934
전준호 <ggwnsgh12@gmail.com>
2026-07-30 14:36:42 +0900

    add quiz

--- codyssey.py
+++ codyssey.py
@@ -1,4 +1,6 @@
 
+import os
+import json
 
 class quiz:
     def __init__(self,question,options,answer):
@@ -15,6 +17,11 @@ class quiz:
 
 class QuizGame:
     def __init__(self,file_name=None):
+        self.filename=self.filename
+        self.quizzes=[]
+        self.high_score=0
+        
+        
         
         if not file_name:
             self.quizzes=[
@@ -25,8 +32,33 @@ class QuizGame:
                     {"question": "논리 연산자 중 '그리고'를 뜻하는 것은?", "options": ["or", "not", "and", "xor"], "answer": 3}
                 ]
         else: self.quizzes=[]
-
-    def get_valid_value(self,prompt,is_numeric=False):
+        
+    def load_data(self):
+        if os.path.exists(self.filename):
+            with open(self.filename,'r',encoding='utf-8') as f:
+                data=json.load(f)
+                self.high_score=data.get('high_score',0)
+                self.quizzes=[quiz(**q) for q in data.get('quizzes',[])]
+        else:
+            initial_data=[
+                                {"question": "파이썬에서 리스트에 요소를 추가하는 함수는?", "options": ["add()", "append()", "insert()", "push()"], "answer": 2},
+                                {"question": "파이썬의 창시자는?", "options": ["Guido van Rossum", "Elon Musk", "Bill Gates", "James Gosling"], "answer": 1},
+                                {"question": "다음 중 불변(Immutable) 자료형은?", "options": ["list", "dict", "set", "tuple"], "answer": 4},
+                                {"question": "출력 함수는?", "options": ["input()", "print()", "write()", "echo()"], "answer": 2},
+                                {"question": "논리 연산자 중 '그리고'를 뜻하는 것은?", "options": ["or", "not", "and", "xor"], "answer": 3}
+                            ]
+            self.quizzes=[quiz(**q) for q in initial_data]
+            self.save_data()
+    def save_data(self):
+        
+        data={
+            'high_score':self.high_score,
+            'quizzes':[q.to_dict() for q in self.quizzes]
+        }
+        with open(self.filename, 'w', encoding='utf-8') as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
+            
+    def get_valid_value(self,prompt,is_numeric=False,):
         while True:
             user_input=input(prompt).strip()
             if not user_input:
@@ -58,6 +90,8 @@ class QuizGame:
         #정답
         answer=self.get_valid_value('정답을 입력해주세요 (숫자 1~4)',True)
         
+        self.quizzes.append(quiz(question,options,answer))
+        self.save_data()
     
     def run(self):
         
@@ -70,15 +104,7 @@ class QuizGame:
             print("2. 퀴즈 추가하기")
             print("3. 종료")
                     
-            try:
-                user_select=int(input("번호 입력:").strip())
-                if user_select=='':
-                    print("다시 입력해 주세요")
-                    continue
-                    
-            except ValueError:
-                print("숫자만 입력 가능합니다.")
-                continue
+            user_select=self.get_valid_value('숫자를 입력해 주세요:',True)
             
             if user_select==1:
                 pass
